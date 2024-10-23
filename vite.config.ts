9b23e05f388d4b3cc07311d51e8c19babe8e8371
GrownUprince <jake.lee4006@gmail.com>
2024-10-23 10:13:35 +0200

    fix: apply tsconfigPaths(to fix build error on Vercel)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from 'path';
+// import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
+  //   resolve: {
+  //     alias: {
+  //       '@': path.resolve(__dirname, './src'),
+  //     },
+  //   },
 });
