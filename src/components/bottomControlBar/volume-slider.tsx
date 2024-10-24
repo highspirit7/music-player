ed30579aef74d6c623ce80cb10f57ccb9fba1c87
GrownUprince <jake.lee4006@gmail.com>
2024-10-24 19:47:37 +0200

    feat: make volume icon clickable

--- src/components/bottomControlBar/volume-slider.tsx
+++ src/components/bottomControlBar/volume-slider.tsx
@@ -1,27 +1,37 @@
 import { Volume2, VolumeOff } from 'lucide-react';
 import { Slider } from '@/components/ui/slider';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { usePlayerStore } from '@/store/usePlayerStore';
 
 function VolumeSlider() {
   const [volume, setVolume] = useState(0.5);
-
+  const volumeCopy = useRef(0);
   const howlInstance = usePlayerStore(state => state.howlInstance);
 
   useEffect(() => {
     howlInstance?.volume(volume);
   }, [volume, howlInstance]);
 
+  useEffect(() => {
+    if (volume !== 0) volumeCopy.current = volume;
+  }, [volume]);
+
   return (
     <div className="flex items-center space-x-2 justify-self-end">
-      {volume === 0 ? (
-        <VolumeOff className="h-5 w-5 text-muted-foreground" />
-      ) : (
-        <Volume2 className="h-5 w-5 text-muted-foreground" />
-      )}
+      <button onClick={() => setVolume(volume === 0 ? volumeCopy.current : 0)}>
+        {volume === 0 ? (
+          <VolumeOff className="h-5 w-5 text-muted-foreground" />
+        ) : (
+          <Volume2 className="h-5 w-5 text-muted-foreground" />
+        )}
 
+        <span className="sr-only">
+          {volume === 0 ? 'Volume Off' : 'Volume On'}
+        </span>
+      </button>
       <Slider
         defaultValue={[volume]}
+        value={[volume]}
         max={1}
         step={0.1}
         onValueChange={value => setVolume(value[0])}
