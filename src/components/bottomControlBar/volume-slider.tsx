40cc98791c658f4bdecc9453bb234326795a64c7
GrownUprince <jake.lee4006@gmail.com>
2024-10-24 16:18:23 +0200

    ui: apply volume off button

--- src/components/bottomControlBar/volume-slider.tsx
+++ src/components/bottomControlBar/volume-slider.tsx
@@ -1,20 +1,25 @@
-import { Volume2 } from 'lucide-react';
+import { Volume2, VolumeOff } from 'lucide-react';
 import { Slider } from '@/components/ui/slider';
 import { useEffect, useState } from 'react';
 import { usePlayerStore } from '@/store/usePlayerStore';
 
 function VolumeSlider() {
   const [volume, setVolume] = useState(0.5);
+
   const howlInstance = usePlayerStore(state => state.howlInstance);
 
   useEffect(() => {
     howlInstance?.volume(volume);
   }, [volume, howlInstance]);
 
-  // TODO : apply 'volume off' button too
   return (
     <div className="flex items-center space-x-2 justify-self-end">
-      <Volume2 className="h-5 w-5 text-muted-foreground" />
+      {volume === 0 ? (
+        <VolumeOff className="h-5 w-5 text-muted-foreground" />
+      ) : (
+        <Volume2 className="h-5 w-5 text-muted-foreground" />
+      )}
+
       <Slider
         defaultValue={[volume]}
         max={1}
